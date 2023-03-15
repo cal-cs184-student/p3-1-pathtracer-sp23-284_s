961054be15a2cd2983982c07475af32e0c3e4f9f
Shu-Ping Chen <shuping_chen@berkeley.edu>
2023-03-15 09:32:09 -0700

    report

--- src/scene/bvh.cpp
+++ src/scene/bvh.cpp
@@ -57,7 +57,7 @@ BVHNode *BVHAccel::construct_bvh(std::vector<Primitive *>::iterator start,
   // size configuration. The starter code build a BVH aggregate with a
   // single leaf node (which is also the root) that encloses all the
   // primitives.
-  /*
+
   BBox bbox, centroid_box;
   for (auto p = start; p != end; p++) {
     BBox bb = (*p)->get_bbox();
@@ -122,123 +122,6 @@ BVHNode *BVHAccel::construct_bvh(std::vector<Primitive *>::iterator start,
   }
   node->l = construct_bvh(start, center_left, max_leaf_size);
   node->r = construct_bvh(center_left, end, max_leaf_size);
-  return node;
-  */
-
-  // Create bounding box
-  BBox bbox;
-  for (auto p = start; p != end; p++) {
-    BBox bb = (*p)->get_bbox();
-    bbox.expand(bb);
-  }
-
-  // Create new node object
-  BVHNode* node = new BVHNode(bbox);
-
-  // Number of primitives
-  int num_primitives = (end - start);
-
-  // Base Case - Create a leaf node
-  if (num_primitives <= max_leaf_size) {
-    node->l = NULL;
-    node->r = NULL;
-    node->start = start;
-    node->end = end;
-  }
-  else {
-    Vector3D mean(0, 0, 0);
-
-    for (auto p = start; p != end; p++) {
-      mean += (*p)->get_bbox().centroid();
-    }
-    mean /= (float)num_primitives;
-
-
-    int min_split_axis = 0;            // Splitting axis with bounding boxes having smallest surface area
-    float min_BBox_heuristic = INFINITY;   // Heuristic for choosing which cartesian axis to perform mean split along
-
-
-    // ---------------- ASSESSING SPLIT AXIS HEURISTIC ---------------------------
-    for (int axis = 0; axis < 3; axis++) {
-
-      // Create and populate left and right primitive vectors
-      std::vector<Primitive*> left_primitives;
-      std::vector<Primitive*> right_primitives;
-
-      for (auto p = start; p != end; p++) {
-        if ((*p)->get_bbox().centroid()[axis] <= mean[axis]) {
-          left_primitives.push_back(*p);
-        }
-        else {
-          right_primitives.push_back(*p);
-        }
-      }
-
-      BBox left_bbox;
-      BBox right_bbox;
-      // build the child bounding boxes
-      for (int i = 0; i < left_primitives.size(); i++) {
-        BBox bb = left_primitives[i]->get_bbox();
-        left_bbox.expand(bb);
-      }
-      for (int j = 0; j < right_primitives.size(); j++) {
-        BBox bb = right_primitives[j]->get_bbox();
-        right_bbox.expand(bb);
-      }
-
-
-      // compute the BBox heuristic for the current splitting axis
-      Vector3D exL = left_bbox.extent;
-      Vector3D exR = right_bbox.extent;
-      float heuristic = left_primitives.size() * (exL.x * exL.y + exL.y * exL.z + exL.z * exL.x)
-        + right_primitives.size() * (exR.x * exR.y + exR.y * exR.z + exR.z * exR.x);
-      if (heuristic < min_BBox_heuristic) {
-        min_BBox_heuristic = heuristic;
-        min_split_axis = axis;
-      }
-    }
-    // ------------------- END HEURISTIC ASSESSMENT -------------------------
-
-
-    // Update start/end pointers and perform the recursive step
-    std::vector<Primitive*> left_primitives;
-    std::vector<Primitive*> right_primitives;
-
-    for (auto p = start; p != end; p++) {
-      //if ((*p)->get_bbox().centroid().z <= mean.z) {
-      if ((*p)->get_bbox().centroid()[min_split_axis] <= mean[min_split_axis]) {
-        left_primitives.push_back(*p);
-      }
-      else {
-        right_primitives.push_back(*p);
-      }
-    }
-    auto center_left = start;
-    auto center_right = start;
-    for (int i = 0; i < num_primitives; i++) {
-      if (i < left_primitives.size()) {
-        *center_left = left_primitives[i];
-        center_left++;
-      }
-      else {
-        *center_right = right_primitives[i - left_primitives.size()];
-      }
-      center_right++;
-    }
-    node->l = construct_bvh(start, center_left, max_leaf_size);
-    node->r = construct_bvh(center_left, end, max_leaf_size);
-
-
-    /*
-    // Sort primitives along x-axis using lambda function comparator
-    sort(start, end, [](const Primitive& p1, const Primitive& p2)
-        {
-            return p1.get_bbox().centroid().x < p2.get_bbox().centroid().x;
-        });
-    */
-
-  }
-
   return node;
 }
 
